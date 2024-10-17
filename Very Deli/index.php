20636d134107cc3ac3ea5ed64849a814144c471f
enzo078 <89792157+enzo078@users.noreply.github.com>
2024-10-17 18:11:41 -0300

    Add files via upload

---
+++ Very Deli/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="contenedor-logo">
+            <img src="./imagenes/LogoDery.png" alt="logo" class="logo">
+            <h1>Very Deli</h1>  
+        </div>       
+        <div class="btns-login">
+            <a class="btn-iniciar" href="./paginas/inicio.php">Iniciar Sesion</a>
+            <a class="btn-registrarse" href="./paginas/registro.php">Registrarse</a>
+        </div>
+    </header>
+    <main>
+        <div class="contenedor-filtro">
+            <h3>Buscar publicacion por:</h3>
+            <div class="filtro-zona">
+                <label for="">Zona:</label>
+                <select name="" id="">
+                    <option value="" selected disabled>Seleccione una zona</option>
+                </select>
+            </div>
+            <div class="filtro-descripcion">
+                <label for="">Descripcion:</label>
+                <input type="text" name="" id="" placeholder="Ingrese la descripcion">
+            </div>
+        </div>
+        <div class="contenedor-lista">
+            <a href="./paginas/publicacion.php">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="publicacion">
+                    <h4>Nombre Necesidad</h4>
+                    <p>Descripcion</p>
+                </div>
+            </a>
+        </div>
+    </main>
+    <footer>
+        <p>Universidad Nacional de San Luis</p>
+        <p>Programacion III</p>
+    </footer>
+</body>
+</html>
---
+++ Very Deli/paginas/registro.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./estilos-iniciar-registro.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="../index.php" class = "vinculo-home">
+            <div class="contenedor-logo">
+                <img src="../imagenes/LogoDery.png" alt="logo" class="logo">
+                <h1>Very Deli</h1>  
+            </div>  
+        </a>
+             
+        <div class="btns-login">
+            <a class="btn-iniciar" href="./inicio.php">Iniciar Sesion</a>
+            <a class="btn-registrarse" href="">Registrarse</a>
+        </div>
+    </header>
+    <main>
+        <div class = "formulario-login">
+            <h2>Crear Sesion</h2>
+            <div class = "contenedor-correo">
+                <label for="">Correo Electronico</label>
+                <input type="text" placeholder="usuario@gmail.com">
+            </div>
+            <div class= "contenedor-contraseña">
+                <label for="">Contraseña</label>
+                <input type="text">
+            </div>
+            <div>
+                <input type="submit" value="Registrarse">
+            </div>
+            
+        </div>
+    </main>
+    <footer>
+        <p>Universidad Nacional de San Luis</p>
+        <p>Programacion III</p>
+    </footer>
+</body>
+</html>
