6c86e6777c5cbb9d689050adf75cea967567f370
jero447 <86937696+jero447@users.noreply.github.com>
2024-10-23 13:18:10 -0300

    Agrego funcionalidad de crear publicacion

--- VeryDeliDB.sql
+++ VeryDeliDB.sql
@@ -1,169 +1,222 @@
--- MySQL dump 10.13  Distrib 5.6.23, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: delivery
--- ------------------------------------------------------
--- Server version	5.5.5-10.1.37-MariaDB
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-10-2024 a las 17:38:53
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `calificacion`
+-- Base de datos: `delivery`
 --
 
-DROP TABLE IF EXISTS `calificacion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificacion`
+--
+
 CREATE TABLE `calificacion` (
-  `idCalificacion` int(11) NOT NULL AUTO_INCREMENT,
-  `descripccion` text,
+  `idCalificacion` int(11) NOT NULL,
+  `descripccion` text DEFAULT NULL,
   `valor` float NOT NULL,
-  `idUsuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idCalificacion`),
-  KEY `idUsuario` (`idUsuario`),
-  CONSTRAINT `calificacion_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `idUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `calificacion`
+-- Estructura de tabla para la tabla `postulacion`
 --
 
-LOCK TABLES `calificacion` WRITE;
-/*!40000 ALTER TABLE `calificacion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `calificacion` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `postulacion`
---
-
-DROP TABLE IF EXISTS `postulacion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `postulacion` (
-  `idPostulacion` int(11) NOT NULL AUTO_INCREMENT,
+  `idPostulacion` int(11) NOT NULL,
   `monto` float NOT NULL,
   `estado` varchar(20) NOT NULL,
   `idPublicacion` int(11) DEFAULT NULL,
-  `idUsuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idPostulacion`),
-  KEY `idPublicacion` (`idPublicacion`),
-  KEY `idUsuario` (`idUsuario`),
-  CONSTRAINT `postulacion_ibfk_1` FOREIGN KEY (`idPublicacion`) REFERENCES `publicacion` (`idPublicacion`),
-  CONSTRAINT `postulacion_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `idUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `postulacion`
+-- Estructura de tabla para la tabla `publicacion`
 --
 
-LOCK TABLES `postulacion` WRITE;
-/*!40000 ALTER TABLE `postulacion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `postulacion` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `publicacion`
---
-
-DROP TABLE IF EXISTS `publicacion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `publicacion` (
-  `idPublicacion` int(11) NOT NULL AUTO_INCREMENT,
+  `idPublicacion` int(11) NOT NULL,
   `volumen` float NOT NULL,
   `peso` float NOT NULL,
   `origen` varchar(30) NOT NULL,
   `destino` varchar(30) NOT NULL,
-  PRIMARY KEY (`idPublicacion`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `idUsuario` int(11) DEFAULT NULL,
+  `titulo` varchar(30) DEFAULT NULL,
+  `descripcion` varchar(70) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `publicacion`
+-- Volcado de datos para la tabla `publicacion`
 --
 
-LOCK TABLES `publicacion` WRITE;
-/*!40000 ALTER TABLE `publicacion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `publicacion` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `publicacion` (`idPublicacion`, `volumen`, `peso`, `origen`, `destino`, `idUsuario`, `titulo`, `descripcion`) VALUES
+(9, 12, 45, 'Cordoba', 'Mendoza', 5, 'Flete ', 'Flete de un armario'),
+(10, 1231, 1231, 'Cordoba', 'Chau', 5, 'chau', 'demasiado texto');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estructura de tabla para la tabla `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
-  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nombre_usuario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `clave` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `dni` int(11) NOT NULL,
-  PRIMARY KEY (`idUsuario`),
-  UNIQUE KEY `dni` (`dni`),
-  UNIQUE KEY `nombre_usuario_UNIQUE` (`nombre_usuario`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `clave` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `dni` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `usuario`
+-- Volcado de datos para la tabla `usuario`
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Ramon Zapico','raramon.240@gmail.com','ramoncho12','$2y$10$Epxsm2hGQ4qiK8RIn/PL/uJUJNFI8/awVyFn10STwnbrmsm7Z7Lf6',43620901),(4,'Estela de Carlotto','raramiro.230@gmail.com','Estee44','$2y$10$Nh9cvPDOUL81J16qtp1KcOCgWpC.VdNMdPkASiYrecIOral4vZfXq',43620904);
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `usuario` (`idUsuario`, `nombre`, `email`, `nombre_usuario`, `clave`, `dni`) VALUES
+(1, 'Ramon Zapico', 'raramon.240@gmail.com', 'ramoncho12', '$2y$10$Epxsm2hGQ4qiK8RIn/PL/uJUJNFI8/awVyFn10STwnbrmsm7Z7Lf6', 43620901),
+(4, 'Estela de Carlotto', 'raramiro.230@gmail.com', 'Estee44', '$2y$10$Nh9cvPDOUL81J16qtp1KcOCgWpC.VdNMdPkASiYrecIOral4vZfXq', 43620904),
+(5, 'Jeronimo Sturniolo', 'jeronimostur@hotmail.com', 'jero447', '$2y$10$hN3gP2.mYjiTQgDrErMBuumDGRFnj97aymj5RqigcytO/399gjXcW', 44358758);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `vehiculo`
+-- Estructura de tabla para la tabla `vehiculo`
 --
 
-DROP TABLE IF EXISTS `vehiculo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vehiculo` (
-  `idVehiculo` int(11) NOT NULL AUTO_INCREMENT,
+  `idVehiculo` int(11) NOT NULL,
   `matricula` varchar(6) NOT NULL,
   `modelo` varchar(20) NOT NULL,
   `color` varchar(20) NOT NULL,
-  `idUsuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idVehiculo`),
-  KEY `idUsuario` (`idUsuario`),
-  CONSTRAINT `vehiculo_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `idUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `vehiculo`
+-- Índices para tablas volcadas
 --
 
-LOCK TABLES `vehiculo` WRITE;
-/*!40000 ALTER TABLE `vehiculo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vehiculo` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD PRIMARY KEY (`idCalificacion`),
+  ADD KEY `idUsuario` (`idUsuario`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indices de la tabla `postulacion`
+--
+ALTER TABLE `postulacion`
+  ADD PRIMARY KEY (`idPostulacion`),
+  ADD KEY `idPublicacion` (`idPublicacion`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD PRIMARY KEY (`idPublicacion`),
+  ADD KEY `fk_usuario_publicaciones` (`idUsuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `nombre_usuario_UNIQUE` (`nombre_usuario`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`);
+
+--
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`idVehiculo`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  MODIFY `idCalificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `postulacion`
+--
+ALTER TABLE `postulacion`
+  MODIFY `idPostulacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  MODIFY `idPublicacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  MODIFY `idVehiculo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD CONSTRAINT `calificacion_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `postulacion`
+--
+ALTER TABLE `postulacion`
+  ADD CONSTRAINT `postulacion_ibfk_1` FOREIGN KEY (`idPublicacion`) REFERENCES `publicacion` (`idPublicacion`),
+  ADD CONSTRAINT `postulacion_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD CONSTRAINT `fk_usuario_publicaciones` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD CONSTRAINT `vehiculo_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-10-19 22:10:54
